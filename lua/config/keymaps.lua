d2cfd509ef1f580a3bbe590735a1b290b0530876
Jankuz <longanh281@gmail.com>
2025-04-09 15:34:13 +0700

    Update backup-setting 2025-04-09 15:34:13

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "<leader>nl", function()
+	local name = vim.fn.input("Library name: ")
+	if name ~= "" then
+		vim.cmd("!dotnet new classlib -n " .. name .. " -o src/" .. name)
+		vim.cmd("!dotnet sln add src/" .. name .. "/" .. name .. ".csproj")
+		vim.cmd("OmniSharpReloadSolution")
+	end
+end, { desc = "Add new class library" })
